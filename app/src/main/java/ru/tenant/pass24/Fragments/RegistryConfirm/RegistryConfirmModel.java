7ceb2c6de7d6ff230279c21b854260084dedc8f8
rustem <bikbulatov_r@ufanet.ru>
2019-12-13 13:24:20 +0500

    rest method realization

--- app/src/main/java/ru/tenant/pass24/Fragments/RegistryConfirm/RegistryConfirmModel.java
+++ app/src/main/java/ru/tenant/pass24/Fragments/RegistryConfirm/RegistryConfirmModel.java
@@ -1,5 +1,40 @@
 package ru.tenant.pass24.Fragments.RegistryConfirm;
 
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import ru.tenant.pass24.Fragments.Registration.apiModels.RegistryCheckRequestBody;
+import ru.tenant.pass24.Fragments.Registration.apiModels.RegistryResponse;
+import ru.tenant.pass24.Helpers.Retrofit.ApiService;
+
 public class RegistryConfirmModel {
     public RegistryConfirmPresenter registryConfirmPresenter;
+
+    public void confirmTelephone(RegistryCheckRequestBody registryCheckRequestBody) {
+        ApiService.getInstance().getAuthApi().registrate(registryCheckRequestBody)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<RegistryResponse>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(RegistryResponse loginResponse) {
+//                        registryConfirmPresenter.toRegistryConfirm();
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        //todo add dialog
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+    }
 }
