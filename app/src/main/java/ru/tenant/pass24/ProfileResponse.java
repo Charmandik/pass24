3fb3ec1c83eb29819244552fea0aca8daefa0517
Charmandik <rbikbulatov@list.ru>
2019-12-24 23:00:22 +0500

    added classes and needed fields in it for get profile method

--- app/src/main/java/ru/tenant/pass24/ProfileResponse.java
+++ app/src/main/java/ru/tenant/pass24/ProfileResponse.java
@@ -1,4 +1,16 @@
 package ru.tenant.pass24;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import ru.tenant.pass24.AuthorizationFragments.Registration.apiModels.RegistryResponseBody;
+
 public class ProfileResponse {
+    @SerializedName("body")
+    @Expose
+    private ProfileResponseBody body;
+
+    @SerializedName("error")
+    @Expose
+    private ProfileResponseError error;
 }
---
+++ app/src/main/java/ru/tenant/pass24/ProfileResponseError.java
@@ -0,0 +1,30 @@
+package ru.tenant.pass24;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+class ProfileResponseError {
+    @SerializedName("message")
+    @Expose
+    private String message;
+
+    @SerializedName("code")
+    @Expose
+    private String code;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
