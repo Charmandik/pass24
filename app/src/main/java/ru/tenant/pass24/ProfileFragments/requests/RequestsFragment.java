ffb90fb4c9c394a99800a93ce14575cf99c938b0
ruslan <arsmarks@gmail.com>
2019-12-26 14:31:42 +0500

    add RequestPagination, requestResponseError

--- app/src/main/java/ru/tenant/pass24/ProfileFragments/requests/RequestsFragment.java
+++ app/src/main/java/ru/tenant/pass24/ProfileFragments/requests/RequestsFragment.java
@@ -45,6 +45,5 @@ public class RequestsFragment extends Fragment {
         layoutManager = new LinearLayoutManager(this.getContext());
         rvMyRequest.setLayoutManager(layoutManager);
 
-
     }
 }
