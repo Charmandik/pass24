e9c46a6e7ef89b58f47abbe574b8de77cdfeef2d
Charmandik <rbikbulatov@list.ru>
2019-12-28 19:08:48 +0500

    passes little bit navigation

--- app/src/main/java/ru/tenant/pass24/ProfileFragments/passes/PassOrderVehicleFragment.java
+++ app/src/main/java/ru/tenant/pass24/ProfileFragments/passes/PassOrderVehicleFragment.java
@@ -4,15 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import ru.tenant.pass24.ProfileFragments.AddressSearchFragment;
+import ru.tenant.pass24.ProfileFragments.ValidityFragment;
+import ru.tenant.pass24.ProfileFragments.VehicleBrandFragment;
 import ru.tenant.pass24.R;
 
 public class PassOrderVehicleFragment extends Fragment {
+    private RelativeLayout rlPassOrderAddress, rlPassOrderType, rlPassOrderVehicleBrand, rlPassOrderVehicleType, rlPassOrderVisitTime;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -26,13 +30,86 @@ public class PassOrderVehicleFragment extends Fragment {
     }
 
     private void init(View view) {
+        rlPassOrderAddress = view.findViewById(R.id.rlPassOrderAddress);
+        rlPassOrderType = view.findViewById(R.id.rlPassOrderType);
+        rlPassOrderVehicleBrand = view.findViewById(R.id.rlPassOrderVehicleBrand);
+        rlPassOrderVehicleType = view.findViewById(R.id.rlPassOrderVehicleType);
+        rlPassOrderVisitTime = view.findViewById(R.id.rlPassOrderVisitTime);
+
+        rlPassOrderAddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toAddressSearchFragment();
+            }
+        });
+
+        rlPassOrderType.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toPassOrderTypeFragment();
+            }
+        });
+
+        rlPassOrderVehicleBrand.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toVehicleBrandFragment();
+            }
+        });
+
+        rlPassOrderVehicleType.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toPassOrderVehicleTypeFragment();
+            }
+        });
+
+        rlPassOrderVisitTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toValidityFragment();
+            }
+        });
     }
 
-    public void toPassOrderFragment() {
+
+    public void toAddressSearchFragment() {
         getFragmentManager()
                 .beginTransaction()
-                .replace(R.id.flPassesContainer, new PassOrderFragment())
-                .addToBackStack(null)
+                .replace(R.id.flPassesContainer, new AddressSearchFragment())
+                .addToBackStack("")
+                .commit();
+    }
+
+    public void toPassOrderTypeFragment() {
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.flPassesContainer, new PassOrderTypeFragment())
+                .addToBackStack("")
+                .commit();
+    }
+
+    public void toVehicleBrandFragment() {
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.flPassesContainer, new VehicleBrandFragment())
+                .addToBackStack("")
+                .commit();
+    }
+
+    public void toPassOrderVehicleTypeFragment() {
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.flPassesContainer, new PassOrderVehicleTypeFragment())
+                .addToBackStack("")
+                .commit();
+    }
+
+    public void toValidityFragment() {
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.flPassesContainer, new ValidityFragment())
+                .addToBackStack("")
                 .commit();
     }
 }
