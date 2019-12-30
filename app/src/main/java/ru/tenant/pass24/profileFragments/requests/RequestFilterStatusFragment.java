8ed6dd69782477353d297d3649c2bc7a5796351d
ruslan <arsmarks@gmail.com>
2019-12-30 18:03:25 +0500

    add checkbox listener

--- app/src/main/java/ru/tenant/pass24/profileFragments/requests/RequestFilterStatusFragment.java
+++ app/src/main/java/ru/tenant/pass24/profileFragments/requests/RequestFilterStatusFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,6 +14,8 @@ import androidx.fragment.app.Fragment;
 import ru.tenant.pass24.R;
 
 public class RequestFilterStatusFragment extends Fragment {
+    private LinearLayout llAll, llOpen, llAccept, llReject;
+    private CheckBox cbAll, cbOpen, cbAccept, cbReject;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -25,6 +29,95 @@ public class RequestFilterStatusFragment extends Fragment {
     }
 
     private void init(View view) {
+        llAll = view.findViewById(R.id.llAllStatusRequest);
+        llOpen = view.findViewById(R.id.llOpenStatusRequest);
+        llAccept = view.findViewById(R.id.llAcceptStatusRequest);
+        llReject = view.findViewById(R.id.llRejectStatusRequest);
+
+        cbAll = view.findViewById(R.id.cbAllStatus);
+        cbOpen = view.findViewById(R.id.cbOpenStatus);
+        cbAccept = view.findViewById(R.id.cbAcceptStatus);
+        cbReject = view.findViewById(R.id.cbRejectStatus);
+
+        llAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cbAll.setChecked(true);
+                cbOpen.setChecked(false);
+                cbAccept.setChecked(false);
+                cbReject.setChecked(false);
+            }
+        });
+
+        llOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cbAll.setChecked(false);
+                cbOpen.setChecked(true);
+                cbAccept.setChecked(false);
+                cbReject.setChecked(false);
+            }
+        });
+
+        llAccept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cbAll.setChecked(false);
+                cbOpen.setChecked(false);
+                cbAccept.setChecked(true);
+                cbReject.setChecked(false);
+            }
+        });
+
+        llReject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cbAll.setChecked(false);
+                cbOpen.setChecked(false);
+                cbAccept.setChecked(false);
+                cbReject.setChecked(true);
+            }
+        });
+
+        cbAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cbAll.setChecked(true);
+                cbOpen.setChecked(false);
+                cbAccept.setChecked(false);
+                cbReject.setChecked(false);
+            }
+        });
+
+        cbOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cbAll.setChecked(false);
+                cbOpen.setChecked(true);
+                cbAccept.setChecked(false);
+                cbReject.setChecked(false);
+            }
+        });
+
+        cbAccept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cbAll.setChecked(false);
+                cbOpen.setChecked(false);
+                cbAccept.setChecked(true);
+                cbReject.setChecked(false);
+            }
+        });
+
+        cbReject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cbAll.setChecked(false);
+                cbOpen.setChecked(false);
+                cbAccept.setChecked(false);
+                cbReject.setChecked(true);
+            }
+        });
 
     }
 }
