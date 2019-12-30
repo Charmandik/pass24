34ba0068e1e9358ed269124b3898a02518fb0501
rustem <bikbulatov_r@ufanet.ru>
2019-12-30 15:25:13 +0500

    new address request logic fixes

--- app/src/main/java/ru/tenant/pass24/profileFragments/requests/newAddress/RequestNewAddressFragment.java
+++ app/src/main/java/ru/tenant/pass24/profileFragments/requests/newAddress/RequestNewAddressFragment.java
@@ -1,6 +1,9 @@
 package ru.tenant.pass24.profileFragments.requests.newAddress;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,7 +12,10 @@ import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.Random;
 
@@ -21,6 +27,7 @@ import ru.tenant.pass24.R;
 import ru.tenant.pass24.helpers.Constants;
 import ru.tenant.pass24.helpers.Retrofit.ApiService;
 import ru.tenant.pass24.profileFragments.requests.RequestTypeFragment;
+import ru.tenant.pass24.profileFragments.requests.RequestsFragment;
 import ru.tenant.pass24.profileFragments.requests.apiModels.createRequestModels.CreateRequestBody;
 import ru.tenant.pass24.profileFragments.requests.apiModels.createRequestModels.CreateRequestData;
 import ru.tenant.pass24.profileFragments.requests.apiModels.createRequestModels.CreateRequestResponse;
@@ -28,6 +35,8 @@ import ru.tenant.pass24.profileFragments.requests.apiModels.createRequestModels.
 public class RequestNewAddressFragment extends Fragment {
     private ImageView btnBack, btnClose;
     private Button btnSendAddress;
+    private TextInputLayout tilNewAddress;
+    private Context mContext;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -38,6 +47,7 @@ public class RequestNewAddressFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         init(view);
+        mContext = this.getContext();
     }
 
     public void toRequestsAdd() {
@@ -52,6 +62,8 @@ public class RequestNewAddressFragment extends Fragment {
         btnBack = view.findViewById(R.id.btnBack);
         btnClose = view.findViewById(R.id.btnClose);
         btnSendAddress = view.findViewById(R.id.btnSendAddress);
+        btnSendAddress.setClickable(false);
+        tilNewAddress = view.findViewById(R.id.tilNewAddress);
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,17 +81,34 @@ public class RequestNewAddressFragment extends Fragment {
         btnSendAddress.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                createNewAddress();
+                if (tilNewAddress.getEditText().getText().length() > 0)
+                    createNewAddress();
             }
         });
-    }
 
-    public void toRequestType() {
-        getFragmentManager()
-                .beginTransaction()
-                .replace(R.id.flRequestsContainer, RequestTypeFragment.getInstance())
-                .addToBackStack(RequestTypeFragment.TAG)
-                .commit();
+        tilNewAddress.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable text) {
+                if (text != null)
+                    if (text.length() > 0) {
+                        btnSendAddress.setClickable(true);
+                        setButtonBackground(R.drawable.rounded_red_button);
+                    } else {
+                        btnSendAddress.setClickable(false);
+                        setButtonBackground(R.drawable.rounded_gray_button);
+                    }
+            }
+        });
     }
 
     public void createNewAddress() {
@@ -87,7 +116,7 @@ public class RequestNewAddressFragment extends Fragment {
         createRequestBody.setType(Constants.objectJoinType);
         Random random = new Random();
 
-        createRequestBody.setRequestData(new CreateRequestData(random.nextInt(), "New address"));
+        createRequestBody.setRequestData(new CreateRequestData(1, tilNewAddress.getEditText().getText().toString()));
         ApiService.getInstance().getRequestApi().createRequest(Constants.getAuthToken(), createRequestBody)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
@@ -104,14 +133,34 @@ public class RequestNewAddressFragment extends Fragment {
 
                     @Override
                     public void onError(Throwable e) {
-                    }
 
+                    }
 
                     @Override
                     public void onComplete() {
-
+                        toRequestsFragment();
                     }
                 });
+    }
+
+    public void setButtonBackground(int drawable) {
+        btnSendAddress.setBackground(ContextCompat.getDrawable(mContext, drawable));
+    }
+
+    public void toRequestType() {
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.flRequestsContainer, RequestTypeFragment.getInstance())
+                .addToBackStack(RequestTypeFragment.TAG)
+                .commit();
+    }
+
+    public void toRequestsFragment() {
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.flRequestsContainer, new RequestsFragment())
+                .addToBackStack(RequestTypeFragment.TAG)
+                .commit();
     }
 
 
